4a2b9b1c68833eb7b71c99b47b2ce29077ea442c
solahidris <emailsolah@gmail.com>
2023-06-09 15:53:16 +0800

    draft steps to code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="text-3xl italic font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-gray-700 h-screen">
+
+      <div className="flex justify-center py-6">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      
+      <div className="text-center mx-[18%] font-mono text-xs text-white">
+        <p className="font-bold pb-4">Random Quote Generator</p>
+        <p>Build a random quote generator app in React where the user can see randomly generated quotes through external API and generates a new quote the button is clicked.</p>
+      </div>
+
+      <div className="mx-[22%] font-mono text-xs text-white mt-5">
+        <p className="text-center italic font-bold">steps</p>
+        <p>1- make title on top for this project</p>
+        <p>2- make quote display label</p>
+        <p>3- make button to create quote</p>
+        <p>4- state for quote. initial "" string</p>
+        <p>5- find api to fetch</p>
+        <p>6- learn axios fetch</p>
+        <p>7- fetch api and store json</p>
+        <p>8- connect api with label</p>
+      </div>
+
+
     </div>
   );
 }
