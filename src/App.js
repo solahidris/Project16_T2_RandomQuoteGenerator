18fcf24cbbd38b8ae4c34cc516f46fdc7a6ed3c5
solahidris <emailsolah@gmail.com>
2023-06-20 17:33:07 +0900

    made it nicer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+
+  const [quoteDisplay, updateQuoteDisplay] = useState("quoteDisplay Initial State");
+  const apiKey = process.env.REACT_APP_APININJAS_API_KEY;
+
   return (
-    <div className="bg-gray-700 h-screen">
+    <div className="bg-gradient-to-t from-gray-900 to-gray-600 h-screen">
 
       <div className="flex justify-center py-6">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,10 +22,10 @@ function App() {
 
       <div className="mx-[22%] font-mono text-xs text-white mt-5">
         <p className="text-center italic font-bold">steps</p>
-        <p>1- make title on top for this project</p>
-        <p>2- make quote display label</p>
-        <p>3- make button to create quote</p>
-        <p>4- state for quote. initial "" string</p>
+        <p>1- make title on top for this project✅</p>
+        <p>2- make quote display label✅</p>
+        <p>3- make button to create quote✅</p>
+        <p>4- state for quote. initial "" string✅</p>
         <p>5- find api to fetch</p>
         <p>6- learn axios fetch</p>
         <p>7- fetch api and store json</p>
@@ -27,15 +33,23 @@ function App() {
         <p>-----------------------------------</p>
       </div>
 
-      {/* { Title } */}
+        {/* { Title } */}
       <div>
-        <p className=" text-center text-sky-300 font-bold text-3xl">Random Quote Generator</p>
+          <label className="flex justify-center text-sky-300 font-bold text-3xl mx-2 py-5">Random Quote Generator</label>
       </div>
 
-      {/* { Display Label } */}
-      <div></div>
+        {/* { Display Label } */}
+      <div className="">
+        <label className="flex justify-center py-5 mx-20 bg-gradient-to-r from-slate-700 to-gray-500 rounded-lg outline outline-slate-500/80 outline-2">{quoteDisplay}</label>
+      </div>
 
+        {/* { Button } */}
+      <div className="flex justify-center">
+        <button className="mt-[3rem] px-4 py-3 bg-blue-500 text-white rounded-md text-sm">Generate New Quote</button>
+      </div>
 
+      <a target="_blank" href="https://www.google.com/search?q=quote+api&oq=quote+api&aqs=chrome..69i57j69i59j69i61.2505j0j7&sourceid=chrome&ie=UTF-8">Click here for API List</a>
+      <p>use API NINJAS</p>
     </div>
   );
 }
