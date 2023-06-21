88062bea8ab58aa0bfc53772c95d6be8bcb4b8b5
solahidris <emailsolah@gmail.com>
2023-06-21 18:00:49 +0900

    made workflow into a component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { useState } from "react";
+import Workflow from "./components/Workflow";
 
 function App() {
 
@@ -15,27 +16,7 @@ function App() {
   return (
     <div className="bg-gradient-to-t from-gray-900 via-gray-800 to-gray-600 h-screen">
 
-      <div className="flex justify-center py-6">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      
-      <div className="text-center mx-[18%] font-mono text-xs text-white">
-        <p className="font-bold pb-4">Random Quote Generator</p>
-        <p>Build a random quote generator app in React where the user can see randomly generated quotes through external API and generates a new quote the button is clicked.</p>
-      </div>
-
-      <div className="mx-[22%] font-mono text-xs text-white mt-5">
-        <p className="text-center italic font-bold">steps</p>
-        <p>1- make title on top for this project✅</p>
-        <p>2- make quote display label✅</p>
-        <p>3- make button to create quote✅</p>
-        <p>4- state for quote. initial "" string✅</p>
-        <p>5- find api to fetch</p>
-        <p>6- learn axios fetch</p>
-        <p>7- fetch api and store json</p>
-        <p>8- connect api with label</p>
-        <p>-----------------------------------</p>
-      </div>
+      <Workflow />
 
         {/* { Title } */}
       <div>
@@ -44,7 +25,7 @@ function App() {
 
         {/* { Display Label } */}
       <div className="">
-        <label className="flex justify-center py-5 mx-20 bg-gradient-to-r from-slate-700 to-gray-500 rounded-lg text-gray-200 outline outline-slate-500/80 outline-2 drop-shadow-lg">{quoteDisplay}</label>
+        <label className="flex justify-center py-5 mx-20 lg:mx-[25rem] bg-gradient-to-r from-slate-700 to-gray-500 rounded-lg text-gray-200 outline outline-slate-500/80 outline-2 drop-shadow-lg">{quoteDisplay}</label>
       </div>
 
         {/* { Button } */}
---
+++ src/components/Workflow.js
@@ -0,0 +1,36 @@
+import logo from "../logo.svg";
+import React from "react";
+
+const Workflow = () => {
+  return (
+    <div>
+      <div className="flex justify-center py-6">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
+      <div className="text-center mx-[18%] font-mono text-xs text-white">
+        <p className="font-bold pb-4">Random Quote Generator</p>
+        <p className="lg:mx-[8rem]">
+          Build a random quote generator app in React where the user can see
+          randomly generated quotes through external API and generates a new
+          quote the button is clicked.
+        </p>
+      </div>
+
+      <div className="mx-[22%] lg:mx-[30%] font-mono text-xs text-white mt-5">
+        <p className="text-center italic font-bold">steps</p>
+        <p>1- make title on top for this project✅</p>
+        <p>2- make quote display label✅</p>
+        <p>3- make button to create quote✅</p>
+        <p>4- state for quote. initial "" string✅</p>
+        <p>5- find api to fetch✅</p>
+        <p>6- learn axios fetch</p>
+        <p>7- fetch api and store json</p>
+        <p>8- connect api with label</p>
+        <p>-----------------------------------</p>
+      </div>
+    </div>
+  );
+};
+
+export default Workflow;
