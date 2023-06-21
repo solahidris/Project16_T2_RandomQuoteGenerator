1215154baa667d84259915877c7425c50c459051
solahidris <emailsolah@gmail.com>
2023-06-21 22:24:28 +0900

    added pic

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import Workflow from "./components/Workflow";
 import axios from "axios";
 
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
+import mainpic from "./assets/mainpic.jpg";
 import AppTitle from "./components/AppTitle";
 import DisplayLabel from "./components/DisplayLabel";
 import ButtonQuote from "./components/ButtonQuote";
@@ -44,8 +45,8 @@ function App() {
 
   return (
     <div className="bg-gradient-to-t from-gray-900 via-gray-800 to-gray-600 min-h-[100vh]">
-      {/* { React Logo } */}
-      <div className="flex justify-center py-6"><img src={logo} className="App-logo" alt="logo" /></div>
+      <div className=""><img src={logo} className="App-logo" alt="logo" /></div>
+      <div className="flex justify-center"><img src={mainpic} className="w-[50%] h-[100%] lg:w-[15%] lg:h-[100%] aspect-square rounded-3xl"/></div>
       {/* <Workflow /> */}
       <AppTitle />
       <p className="text-white text-xs text-center">External API Quote Fetch via Axios</p>
